c86b4b0d505da786fd937e251ee6cdc5ab2bca8c
suliman youssef <sulimanyoussef05@gmail.com>
2025-03-31 17:03:26 +0300

    advanced validation for product

--- utils/validators/productValidator.js
+++ utils/validators/productValidator.js
@@ -1,23 +1,35 @@
 const { param,body } = require("express-validator");
 const validatorMiddleware = require("../../middlewares/validatorMiddleware");
+const CategoryModel=require('../../models/categoryModel');
+const SubCategoryModel=require('../../models/subCategoryModel');
 
 exports.createProductValidator=[
     body('title')
     .notEmpty().withMessage('product required')
     .isLength({min:3}).withMessage('too short product name'),
+
+
     body('description')
     .notEmpty().withMessage('description required')
     .isLength({max:2000}).withMessage('too long description'),
+
+
     body('quantity')
     .notEmpty().withMessage('quantity required')
     .isNumeric().withMessage('quantity must be a number'),
+
+
     body('sold')
     .optional()
     .isNumeric().withMessage('sold must be a number'),
+
+
     body('price')
     .notEmpty().withMessage('price required')
     .isNumeric().withMessage('price must be a number')
     .isLength({max:32}).withMessage('too long price'),
+
+
     body('priceAfterDiscount')
     .optional()
     .toFloat()
@@ -28,28 +40,67 @@ exports.createProductValidator=[
         }
         return true
     }),
+
+
     body('colors')
     .optional()
     .isArray().withMessage('colors must be an array '),
+
+
     body('imageCover')
     .notEmpty().withMessage('imageCover required'),
+
+
     body('images')
     .optional()
     .isArray().withMessage('images must be an array '),
+
+
     body('category')
     .notEmpty().withMessage('category required')
-    .isMongoId().withMessage('category id must be a valid mongoo id'),
+    .isMongoId().withMessage('category id must be a valid MongoDB ID')
+    .custom(async (categoryId) => {
+        const category = await CategoryModel.findById(categoryId);
+        if (!category) {
+            return Promise.reject(new Error('No category found for this ID'));
+        }
+        return true;
+    }),
+
+
     body('subCategory')
     .optional()
-    .isMongoId().withMessage('subCategory id must be a valid mongoo id'),
+    .isMongoId().withMessage('subCategory id must be a valid mongoo id')
+    .custom(async (subCategoriesIds) => {
+        const foundSubCategories  = await SubCategoryModel.find({ _id: { $in: subCategoriesIds } });
+        if (foundSubCategories.length !== subCategoriesIds.length ) {
+            return Promise.reject(new Error('unvalid subCategories IDs'));
+        }
+        return true;
+    })
+    .custom(async (subCategoriesIds , {req}) => {
+        const SubCategoriesOfCategory  = await SubCategoryModel.find({category: req.body.category});
+        const subCategoriesOfCategoryIds = SubCategoriesOfCategory.map(sub => sub._id.toString());
+        const isValid = subCategoriesIds.every(id => subCategoriesOfCategoryIds.includes(id.toString()));
+        if (!isValid) {
+            return Promise.reject(new Error("sub category must be belong to category"));
+        }
+        return true;
+    }),
+
+
     body('brand')
     .optional()
     .isMongoId().withMessage('brand id must be a valid mongoo id'),
+
+
     body('ratingsAverage')
     .optional()
     .isNumeric()
     .isLength({min:1}).withMessage('min rating average is 1')
     .isLength({max:5}).withMessage('max rating average is 5'),
+
+
     body('ratingsQuantity')
     .optional()
     .isNumeric().withMessage('ratingsQuantity must be a number'),
